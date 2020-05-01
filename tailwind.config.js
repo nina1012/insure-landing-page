3852f547e8fa85105d6fe08d0a7199b026f00c59
nina1012 <labferment10@gmail.com>
2020-05-01 18:27:50 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {}
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
+  plugins: []
+};
